cf86b904ff0a8daa06424c9610301d35ffee2f0c
ParkJinUk4637 <noticepju@gmail.com>
2023-12-31 21:01:48 +0900

    static, media

--- edm_member/edm_user/settings.py
+++ edm_member/edm_user/settings.py
@@ -168,11 +168,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# 정적 파일 설정
 STATIC_URL = '/static/'
-STATIC_ROOT = '/app/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# 미디어 파일 설정
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/app/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
